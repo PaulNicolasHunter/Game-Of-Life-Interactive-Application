1e65a83fa0db983168b62749fdd8298f73a76160
PaulNicolasHunter <choudhary.vivek98@gmail.com>
2019-04-05 15:40:37 +0530

    fixed typo setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='arcade_life',
 	  version='0.1',
 	  description='An interactive implementaion of of Game Of Life',
 	  url='https://github.com/PaulNicolasHunter/Game-Of-Life-Interactive-Application',
-	  author='Vivek Choudhary (PaulNicoalsHunter)',
+	  author='Vivek Choudhary (PaulNicolasHunter)',
 	  author_email='choudhary.vivek98@gmail.com',
 	  license='MIT')
